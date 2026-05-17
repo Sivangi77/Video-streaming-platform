a86524657d1e14e79ec69fc906dce94d93e5e305
Sivangi77 <sivangikashyap9@gmail.com>
2026-05-17 16:55:16 +0530

    Latest changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection failed!!!", err);
+})
+
