8e54ac56cecb537612e06c109abdb517592f0c90
Sivangi77 <sivangikashyap9@gmail.com>
2026-05-23 14:21:43 +0530

    Register user done

--- src/app.js
+++ src/app.js
@@ -10,9 +10,14 @@ app.use(cors({
 }));
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
 app.use(cookieParser());
+
+//routes
+
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
 
 export {app};
